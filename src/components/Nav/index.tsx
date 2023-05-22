6577ed52e568f45117281213b08af37191ac9ae8
JoHaeun <gkdmsdl0727@gmail.com>
2023-05-22 21:46:58 +0900

    nav menu 구성

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,37 @@
+import { NavLink } from "react-router-dom";
+
+import { ROOT } from "../../constants/path";
+
+type pathType = {
+  [key: string]: string;
+};
+
 const Nav = () => {
-  return <div>Nav</div>;
+  const menus: string[] = ["All", "Collections", "Singles"];
+
+  const menusPath: pathType = {
+    All: ROOT.ALL,
+    Collections: ROOT.COLLECTIONS,
+    Singles: ROOT.SINGLES,
+  };
+
+  return (
+    <ul className="nav">
+      {menus.map((item: string, idx: number) => {
+        return (
+          <li key={idx} className="nav-item">
+            <NavLink
+              className={({ isActive }) =>
+                "nav-link" + (isActive ? " activated" : "")
+              }
+              to={menusPath[item]}
+            >
+              {item}
+            </NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 export default Nav;
